f964fd0ba550441fc0cb55710f9aec742326f3b1
Srinidhi <sribhat98@gmail.com>
2020-05-19 07:25:32 +0530

    Update train1.sql

--- train1.sql
+++ train1.sql
@@ -1,2 +1,26 @@
-SELECT * FROM irctc.trains;
-ALTER TABLE trains auto_increment=1023
+create table trains(
+   trainid int NOT NULL auto_increment,
+    trainname VARCHAR(255),
+    activedays VARCHAR(255),
+    locopilot VARCHAR(255),
+    originstation VARCHAR(255),
+    destinationstation VARCHAR(255),
+    pickupstation VARCHAR(255),
+    journeytime TIME NOT NULL,
+	primary key(trainid)
+);
+ALTER TABLE trains auto_increment=1023;
+
+INSERT INTO irctc.trains(`trainname`,`activedays`,`locopilotID`,`originstation`,`destinationstation`,`pickupstation`,`journeytime`)
+values
+('Amritha Express','Daily',100342,' Thiruvananthapuram Central','Palakkad Town','Kollam,Kottayam,Ernakulam,Aluva,Thrissur,Palakkad,Dindigul','00:15:45'),
+('Bandra Terminus UdaipurExpress','Sun,Wed,Fri',100343,' Bandra Terminus',' Udaipur','Bandra,Mumbai,Rajsthan,Udaipur','00:16:55'),
+('Chamundi Express','Daily',100344,' Bangalore City ','Mysore City','Kengeri,Mandya,Pandavpura','00:03:05'),
+('Deccan Express','Daily',100345,' Mumbai CST',' Pune Junction ','Thane,Kalyan,Karjur','00:04:05'),
+('Ernakulam-BangaloreExpress','Daily',100346,'Bangalore City','Ernakulam','BloreCant,Carmelaram,KRpuram','00:09:00'),
+('Flying Ranee','Daily',100347,'Mumbai City','Surat','Andheri,Borivalli,Vapi','00:04:40'),
+('Goa Express','Daily',100348,'Goa','New Delhi',' Margao,Gwalior, Agra ','00:39:25'),
+('Hyderabad Mumbai Express','Daily',100349,'Hyderabad','Mumbai','Wadi,Solapur,Pune','00:15:20'),
+('Indore - Jammu Tawi Weekly Superfast Express ','Mon,Wed',100350,'Indore','Jammu','Kota,Panipat,Ludhiana','00:25:30'),
+('Jaipur Mysore Superfast Express','Thurs,Sat',100351,'Jaipur','Mysore','Kota,Hyderabad,Bangalore City','00:44:00');
+
