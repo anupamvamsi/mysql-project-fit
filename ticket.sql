8c8680c9a6aa59dddce95ad13af823780ba19be0
anupamvamsi <anupam.ismav@gmail.com>
2020-05-19 09:46:04 +0530

    updated ticket.sql

--- ticket.sql
+++ ticket.sql
@@ -3,18 +3,19 @@ CREATE TABLE `ticket` (
     `dateofjourney` datetime NOT NULL,
 	`boardingstation` varchar(45) DEFAULT NULL,
 	`destinationstation` varchar(45) NOT NULL,
-	`tbaseprice` int DEFAULT 90,
+	`tprice` int DEFAULT 90,
 	`ttax` float DEFAULT 0.18,
-	`ticketcost` float,
+	`ticketcost` float GENERATED ALWAYS AS (tprice + (tprice * ttax)) STORED,
 	`tbookingstatus` varchar(20),
 	`coachclass` varchar(10),
-	PRIMARY KEY (`pnrnumber`),
-    FOREIGN KEY (`trainid`) REFERENCES train (`trainid`),
-    FOREIGN KEY (`trainid`) REFERENCES train (`trainid`),
+	PRIMARY KEY (`pnrnumber`)
 );
--- (tbaseprice + (tbaseprice * ttax))
-alter TABLE ticket auto_increment=1883332598;
 
-INSERT INTO `irctc`.`ticticketket` (`dateofjourney`, `boardingstation`, `destinationstation`, `ticketcost`, `tbookingstatus`, `coachclass`) 
-VALUES ('2020-12-12 12:12:12', ' Thiruvananthapuram Central', 'Goa', 1520.00, 'RAC', 'SLEEPER');
+ALTER TABLE ticket auto_increment=1883332598;
+
+INSERT INTO `irctc`.`ticket` (`dateofjourney`, `boardingstation`, `destinationstation`, `tprice`, `tbookingstatus`, `coachclass`) 
+	VALUES
+	(
+		 ('2020-12-12 12:12:12', 'Thiruvananthapuram Central', 'Goa', '300', 'RAC', 'SLEEPER')
+	);
 SELECT * FROM ticket;
